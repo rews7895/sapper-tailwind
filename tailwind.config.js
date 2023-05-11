97e9c380304111bfd3845238247907f1d8e6d8a5
Junseok <yalvz@naver.com>
2023-05-11 21:05:41 +0900

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./**/*.{html,js.svelte}"],
-  theme: {
-    extend: {},
+  darkMode: "media",
+  variants: {
+    extend: {
+      textOpacity: ["dark"],
+    },
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        // you can either spread `colors` to apply all the colors
+        ...colors,
+        // or add them one by one and name whatever you want
+        amber: colors.amber,
+        emerald: colors.emerald,
+        "logo-first": "#009688",
+        "logo-second": "#ffa808",
+        "logo-third": "#022f43",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
